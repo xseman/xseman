02c55c994334f59b930d92f5024ee0f9c519521c
Filip Seman <filip.seman@protonmail.com>
2022-04-07 18:12:33 +0200

    scripts: icon to svg update

--- icons/convert.sh
+++ icons/convert.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+for cmd in {rsvg-convert,}; do
+    if ! command -v "${cmd}" > /dev/null; then
+        echo "This script requires '${cmd}' to be installed."
+        exit 1
+    fi
+done
+
 TEMP="$(mktemp -d)" || exit
 
 for svg in *.svg; do
